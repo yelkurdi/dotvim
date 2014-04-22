5b261b1e1b56737a2ddf58cd80d8e409b0096a25
yelkurdi <yousef.el-kurdi@mail.mcgill.ca>
2014-04-22 12:13:18 -0400

    Add bulk package installer command in install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #
+PKGLIST="texlive vim-latexsuite exuberant-ctags"
+sudo apt-get install $PKGLIST
 
 cd ~/.vim
 ln -sf ~/.vim/vimrc ~/.vimrc
 ln -sf ~/.vim/gvimrc ~/.gvimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -u ~/.vim/bundles.vim +BundleInstall +q
-#sudo apt-get install exuberant-ctags
 cd ~/.vim/tags/
 tar xjf cpp_src.tar.bz2
 ./runCtags.sh
 rm -rf cpp_src
 cd ..
-#sudo apt-get install vim-latexsuite
-#sudo vim-addons -w install latex-suite
-
+sudo vim-addons -w install latex-suite
