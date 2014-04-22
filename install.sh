c493c81676fe45e81ae71e1da254de650f0e07f8
yelkurdi <yousef.el-kurdi@mail.mcgill.ca>
2014-04-22 11:50:33 -0400

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#
+
+cd ~/.vim
+ln -sf ~/.vim/vimrc ~/.vimrc
+ln -sf ~/.vim/gvimrc ~/.gvimrc
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim -u ~/.vim/bundles.vim +BundleInstall +q
+sudo apt-get install exuberant-ctags
+cd ~/.vim/tags/
+tar xjf cpp_src.tar.bz2
+./runCtags.sh
+rm -rf cpp_src
+cd ..
+sudo apt-get install vim-latexsuite
+sudo vim-addons -w install latex-suite
+
