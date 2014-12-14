1fd84063c1ab3a9911fefa2e3c85dad44fe528dd
yelkurdi <yousef.el-kurdi@mail.mcgill.ca>
2014-12-14 16:42:03 -0500

    Removed exuberant ctags, since it is replaced with eclimd

--- setup.sh
+++ setup.sh
@@ -3,16 +3,14 @@ echo "Executing setup"
 CWD_MYVAR=$(pwd)
 echo "PWD: " ${CWD_MYVAR}
 set -o verbose
-cd ~/.vim
-ln -sf ~/.vim/vimrc ~/.vimrc
-ln -sf ~/.vim/gvimrc ~/.gvimrc
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim -u ~/.vim/bundles.vim +PluginInstall +q +q
+VIMDIR=~/dotvim
+cd ${VIMDIR} 
+ln -sf ${VIMDIR}/vimrc ~/.vimrc
+ln -sf ${VIMDIR}/gvimrc ~/.gvimrc
+git clone https://github.com/gmarik/vundle.git ${VIMDIR}/bundle/vundle
+vim -u ${VIMDIR}/bundles.vim +PluginInstall +q +q
 
 # for vim-latex to work
 #ln -fs /home/yelkurdi/.vim/bundle/vim-latex/compiler compiler
-cd ~/.vim/tags/
-tar xjf cpp_src.tar.bz2
-./runCtags.sh
-rm -rf cpp_src
+
 cd $CWD_MYVAR
